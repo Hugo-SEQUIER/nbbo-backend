6acf91e586a78da06b9c3b3a113c676eab335a99
Hugo Sqr <hugosequier@gmail.com>
2025-09-22 08:34:48 +0900

    scale backend

--- run.py
+++ run.py
@@ -1,9 +1,8 @@
 import uvicorn
-from src.app import create_app
 
 def main():
-    app = create_app()
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    # Use import string for workers to work properly
+    uvicorn.run("src.app:create_app", host="0.0.0.0", port=8060, workers=4, factory=True)
 
 if __name__ == "__main__":
     main()
